ec5ef022b690257373d651cd4a45df3ec23d1444
Jonathan Arrance <jonathan@transcirrus.com>
2013-10-03 19:23:25 -0400

    added a new make.sh file

--- make.sh
+++ make.sh
@@ -1,16 +1,17 @@
-mkdir -p /usr/lib/python2.7/dist-packages/transcirrus/
+mkdir -p /usr/local/lib/python2.7/dist-packages/transcirrus/
 echo 'Adding common to Transcirrus dir'
-cp -Rf ./common /usr/lib/python2.7/dist-packages/transcirrus/
+cp -Rf ./common /usr/local/lib/python2.7/dist-packages/transcirrus/
 echo 'Adding component to Transcirrus dir'
-cp -Rf ./component /usr/lib/python2.7/dist-packages/transcirrus/
+cp -Rf ./component /usr/local/lib/python2.7/dist-packages/transcirrus/
 echo 'Adding core to Transcirrus dir'
-cp -Rf ./core /usr/lib/python2.7/dist-packages/transcirrus/
+cp -Rf ./core /usr/local/lib/python2.7/dist-packages/transcirrus/
 echo 'Adding database to Transcirrus dir'
-cp -Rf ./database /usr/lib/python2.7/dist-packages/transcirrus/
+cp -Rf ./database /usr/local/lib/python2.7/dist-packages/transcirrus/
 echo 'Adding tasks to Transcirrus dir'
-cp -Rf ./tasks /usr/lib/python2.7/dist-packages/transcirrus/
+cp -Rf ./tasks /usr/local/lib/python2.7/dist-packages/transcirrus/
 echo 'Adding ha to Transcirrus dir'
-cp -Rf ./ha /usr/lib/python2.7/dist-packages/transcirrus/
+cp -Rf ./ha /usr/local/lib/python2.7/dist-packages/transcirrus/
+cp -f __init__.py /usr/local/lib/python2.7/dist-packages/transcirrus/
 
 #check to see if the log file exists
 if [ -e /var/log/caclogs/system.log ]
