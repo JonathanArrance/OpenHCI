3ef88b81c5d89dd93d107fefaed22c6aa14ccff1
Nick <nick.b@transcirrus.com>
2013-11-29 12:31:25 -0500

    update_image v1 implemented and tested

--- unittests/glance_test.py
+++ unittests/glance_test.py
@@ -22,15 +22,37 @@ b = c.get_auth()
 print "Image"
 i= glance_ops(b)
 print i
+print
 
 print "List Images"
 li = i.list_images()
 print li
+print
 
 print "Get Image %s" % (str(li[1]['image_id']))
 gi = i.get_image(str(li[1]['image_id']))
 print gi
+print
 
+name = gi['image_name']
+
+print "Update Image %s" % (str(li[1]['image_id']))
+update = [
+            {"x-image-meta-name": "test"}
+         ]
+update_dict = {"image_id": (str(li[1]['image_id'])), "update": update}
+ui = i.update_image(update_dict)
+print ui
+print
+
+print "Update Image %s" % (str(li[1]['image_id']))
+update = [
+            {"x-image-meta-name": "cirros-0.3.1-x86_64-uec-ramdisk"}
+         ]
+update_dict = {"image_id": (str(li[1]['image_id'])), "update": update}
+ui = i.update_image(update_dict)
+print ui
+print
 
 #print "Instantiating authorization object for an admin"
 #e = authorization("jonadmin","test")
