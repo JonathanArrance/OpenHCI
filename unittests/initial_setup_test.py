f37c2a80fca9bdb6d3129454c6b25f0ea33ef5f9
Jonathan Arrance <jonathan@transcirrus.com>
2013-12-08 20:39:03 -0500

    unittest

--- unittests/initial_setup_test.py
+++ unittests/initial_setup_test.py
@@ -10,9 +10,10 @@ from transcirrus.operations.initial_setup import run_setup
 auth = authorization("admin","password")
 b = auth.get_auth()
 
-new_system_variables = [{"system_name":"ciac-19974","parameter":"api_ip","param_value":"192.168.10.252"},{"system_name":"ciac-19974","parameter":"mgmt_ip","param_value":"192.168.10.252"},
-    {"system_name":"ciac-19974","parameter":"admin_api_ip","param_value":"192.168.10.252"},{"system_name":"ciac-19974","parameter":"int_api_ip","param_value":"192.168.10.252"},
-    {"system_name":"ciac-19974","parameter":"uplink_ip","param_value":"192.168.10.252"}]
+new_system_variables = [{"system_name":"ciac-25466","parameter":"api_ip","param_value":"192.168.10.247"},{"system_name":"ciac-25466","parameter":"mgmt_ip","param_value":"192.168.10.247"},
+    {"system_name":"ciac-25466","parameter":"admin_api_ip","param_value":"192.168.10.247"},{"system_name":"ciac-25466","parameter":"int_api_ip","param_value":"192.168.10.247"},
+    {"system_name":"ciac-25466","parameter":"uplink_ip","param_value":"192.168.10.247"},{"system_name":"ciac-25466","parameter":"uplink_dns","param_value":"8.8.8.8"},
+    {"system_name":"ciac-25466","parameter":"uplink_gateway","param_value":"192.168.10.1"},{"system_name":"ciac-25466","parameter":"domain_name","param_value":"rtp.transcirrus.com"}]
 
 yo = run_setup(new_system_variables,b)
 
