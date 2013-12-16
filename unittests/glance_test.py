864989bb664a9d6c3861a1b860992b03f8f7c9db
Nick <nick.b@transcirrus.com>
2013-12-16 14:28:30 -0500

    Almost done with Glance

--- unittests/glance_test.py
+++ unittests/glance_test.py
@@ -16,27 +16,27 @@ from transcirrus.component.glance.glance_ops import glance_ops
 print "Instantiating authorization object for an default admin"
 c= authorization("admin","builder")
 
-print "Get admin authorization dictionary"
+print "Get admin authorization dictionary..."
 b = c.get_auth()
 
-print "Image"
+print "Image..."
 i= glance_ops(b)
 print i
 print
 
-print "List Images"
+print "List Images..."
 li = i.list_images()
 print li
 print
 
-print "Get Image %s" % (str(li[1]['image_id']))
+print "Get Image %s..." % (str(li[1]['image_id']))
 gi = i.get_image(str(li[1]['image_id']))
 print gi
 print
 
 name = gi['image_name']
 
-print "Update Image %s" % (str(li[1]['image_id']))
+print "Update Image %s..." % (str(li[1]['image_id']))
 update = [
             {"x-image-meta-name": "test"}
          ]
@@ -45,7 +45,7 @@ ui = i.update_image(update_dict)
 print ui
 print
 
-print "Update Image %s" % (str(li[1]['image_id']))
+print "Update Image %s..." % (str(li[1]['image_id']))
 update = [
             {"x-image-meta-name": "cirros-0.3.1-x86_64-uec-ramdisk"}
          ]
@@ -54,9 +54,31 @@ ui = i.update_image(update_dict)
 print ui
 print
 
-#print "Instantiating authorization object for an admin"
-#e = authorization("jonadmin","test")
+print "Create Image Container..."
+create = [
+            {"x-image-meta-name": "TEST1"}
+         ]
+create_dict = {"create": create}
+ci = i.create_image(create_dict)
+print ci
+print
 
-#print "Get admin authorization dictionary"
-#f = e.get_auth()
-#print f
+print "List Images..."
+li = i.list_images()
+print li
+print
+
+print "Get Image %s..." % (str(li[0]['image_id']))
+gi = i.get_image(str(li[1]['image_id']))
+print gi
+print
+
+print "Delete Image %s..." % (str(li[0]['image_id']))
+di = i.delete_image(str(li[0]['image_id']))
+print di
+print
+
+print "List Images..."
+li = i.list_images()
+print li
+print
