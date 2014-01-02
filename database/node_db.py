c6b97da1407d70bab8c0f7edb842a32cad0bd336
shashaa <shashaa.k@transcirrus.com>
2014-01-02 00:04:49 -0500

    node insertion chnages

--- database/node_db.py
+++ database/node_db.py
@@ -89,7 +89,9 @@ def insert_node(input_dict):
     #make sure none of the values are empty
     for key, val in input_dict.items():
         #skip over these
-        if((key == 'node_nova_zone') or (key == 'node_iscsi_iqn') or (key == 'node_swift_ring') or key == 'cloud_name'):
+        if((key == 'node_nova_zone') or (key == 'node_iscsi_iqn') or \
+                (key == 'node_swift_ring') or (key == 'cloud_name') or \
+                (key == 'node_mgmt_ip') or (key == 'node_controller')):
             continue
         if(val == ""):
             logger.sys_error("The value %s was left blank" %(val))
