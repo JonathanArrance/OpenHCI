f0f32a780945eb48dd515112faf984bae7ab597a
Nick Brust <nick.b@transcirrus.com>
2014-01-16 16:29:10 -0500

    removed scripts

--- interfaces/shell/permissions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-usermod -a -G postgres admin
-usermod -a -G nova admin
-usermod -a -G cinder admin
-usermod -a -G glance admin
-usermod -a -G swift admin
-usermod -a -G quantum admin
-usermod -a -G keystone admin
