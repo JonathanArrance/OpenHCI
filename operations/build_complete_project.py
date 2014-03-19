db299af36a44773c1f7f299da7554cbd65d226dd
Nick Brust <nick.b@transcirrus.com>
2014-03-19 17:42:01 -0400

    try/excepts added

--- operations/build_complete_project.py
+++ operations/build_complete_project.py
@@ -43,56 +43,56 @@ def build_project(auth_dict, project_dict):
         proj = tenant.create_tenant(project_dict['project_name'])
         logger.sys_info("Created project with project name: %s" % project_dict['project_name'])
     except Exception as e:
-        logger.sys_error("Couldn't create a project, %e") %(str(e))
+        logger.sys_error("Couldn't create a project, %e") %e
         
     try:
         project_dict['user_dict']['project_id'] = proj
         pu = user.create_user(project_dict['user_dict'])
         logger.sys_info("Created power user named %s for project named %s " % (project_dict['user_dict']['username'], project_dict['project_name']))
     except Exception as e:
-        logger.sys_error("Couldn't create a power user for the project, %e") %(str(e))
+        logger.sys_error("Couldn't create a power user for the project, %e") %e
         
     try:
         net_dict = {'net_name': project_dict['net_name'],'admin_state':"true", 'shared':"true",'project_id':proj}
         net = neutron_net.add_private_network(net_dict)
         logger.sys_info("Created private netowrk with net name: %s" % project_dict['net_name'])
     except Exception as e:
-        logger.sys_error("Couldn't create a private network, %e") %(str(e))
+        logger.sys_error("Couldn't create a private network, %e") %e
     
     try:
         subnet_dict = {'net_id': net['net_id'],'subnet_dhcp_enable':'true','subnet_dns': project_dict['subnet_dns']}
         subnet = neutron_net.add_net_subnet(subnet_dict)
         logger.sys_info("Created subnet with subnet dns: %s" % project_dict['subnet_dns'])
     except Exception as e:
-        logger.sys_error("Couldn't create a subnet, %e") %(str(e))
+        logger.sys_error("Couldn't create a subnet, %e") %e
         
     try:
         project_dict['sec_group_dict']['project_id'] = proj
         sec_group = nova.create_sec_group(project_dict['sec_group_dict'])
         logger.sys_info("Created security group")
     except Exception as e:
-        logger.sys_error("Couldn't create a security group, %e") %(str(e))
+        logger.sys_error("Couldn't create a security group, %e") %e
         
     try:
         sec_keys_dict = {'key_name': project_dict['sec_keys_name'], 'project_id': proj}
         sec_keys = nova.create_sec_keys(sec_keys_dict)
         logger.sys_info("Created security keys")
     except Exception as e:
-        logger.sys_error("Couldn't create security keys, %e") %(str(e))
+        logger.sys_error("Couldn't create security keys, %e") %e
         
     try:
         router_dict = {'router_name': project_dict['router_name'], 'project_id': proj}
         router = neutron_router.add_router(router_dict)
         logger.sys_info("Created router with router name: %s" % project_dict['router_name'])
     except Exception as e:
-        logger.sys_error("Couldn't create a router, %e") %(str(e))
+        logger.sys_error("Couldn't create a router, %e") %e
         
     try:
         inside_port_dict = {'router_id': router['router_id'], 'subnet_id': subnet['subnet_id'], 'project_id': proj}
         inside_port = neutron_router.add_router_internal_interface(inside_port_dict)
         logger.sys_info("Created router internal interface")
     except Exception as e:
-        logger.sys_error("Couldn't create a router internal interface, %e") %(str(e))
+        logger.sys_error("Couldn't create a router internal interface, %e") %e
         
     try:
         ext_net_dict = neutron_net.list_external_networks()
@@ -105,6 +105,6 @@ def build_project(auth_dict, project_dict):
         outside_port = neutron_router.add_router_gateway_interface(outside_port_dict)
         logger.sys_info("Created router gateway")
     except Exception as e:
-        logger.sys_error("Couldn't create a router gateway, %e") %(str(e))
+        logger.sys_error("Couldn't create a router gateway, %e") %e
 
     return proj
