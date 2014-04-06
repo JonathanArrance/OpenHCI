b5dadcf22c670cce7ef4d174c4fa89e06bd30b9b
Shashaa <shashaankar61@gmail.com>
2014-04-06 17:00:39 -0400

    bug fix for status_ready/status_halt

--- core/cn_sn_client.py
+++ core/cn_sn_client.py
@@ -595,7 +595,8 @@ def processComputeConfig(sock, node_id):
     if post_install_status == True:
 
         # send node_ready status message to cc
-        core_util.send_data(pickle.dumps(core_util.status_ready, -1), sock)
+        status_ready_pkt =  pickle.dumps(core_util.status_ready, -1)
+        core_util.send_data(status_ready_pkt, sock)
 
         # listen for ok message, ack -- loops infinetly
         while True:
@@ -646,7 +647,8 @@ def processComputeConfig(sock, node_id):
         if post_install_status != True:
 
             # send node_halt status message to cc
-            core_util.send_data(pickle.dumps(core_util.status_halt, -1), sock)
+            status_halt_pkt = pickle.dumps(core_util.status_halt, -1)
+            core_util.send_data(status_halt_pkt, sock)
 
             # listen for ok message, ack -- loop infinetly
             while True:
@@ -666,7 +668,8 @@ def processComputeConfig(sock, node_id):
         else:
              
             # send node_ready status message to cc
-            core_util.send_data(pickle.dumps(core_util.status_ready, -1), sock)
+            status_ready_pkt = pickle.dumps(core_util.status_ready, -1)
+            core_util.send_data(status_ready_pkt, sock)
 
             # listen for ok message, ack -- loops infinetly
             while True:
@@ -807,7 +810,8 @@ def processStorageConfig(sock, node_id):
 
     if post_install_status == True:
         # send node ready status to cc
-        core_util.send_data(pickle.dumps(core_util.status_ready, -1), sock)
+        status_ready_pkt = pickle.dumps(core_util.status_ready, -1)
+        core_util.send_data(status_ready_pkt, sock)
 
         # listen for ok ack message -- loop infinetly
         while True:
@@ -873,7 +877,8 @@ def processStorageConfig(sock, node_id):
                         print "node_id: %s listening for status_halt ack" % (node_id)
         else:
             # send node ready status message to cc
-            core_util.send_data(pickle.dumps(core_util.status_ready, -1), sock)
+            status_ready_pkt = pickle.dumps(core_util.status_ready, -1)
+            core_util.send_data(status_ready_pkt, sock)
 
             # listen for ok ack message
             while True:
