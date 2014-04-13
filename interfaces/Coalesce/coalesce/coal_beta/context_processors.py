aa7163ce4ae5ff18c45b09cbb8c9a27ce7e02cbe
Nick Brust <nick.b@transcirrus.com>
2014-04-13 19:13:33 -0400

    nick beginning on web interface

--- interfaces/Coalesce/coalesce/coal_beta/context_processors.py
+++ interfaces/Coalesce/coalesce/coal_beta/context_processors.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from transcirrus.component.keystone.keystone_tenants import tenant_ops
 from transcirrus.database.node_db import list_nodes
+from transcirrus.common import node_util
 
 def global_vars(request):
     try:
@@ -9,15 +10,19 @@ def global_vars(request):
         username = auth['username']
         user_level = auth['user_level']
         project_id = auth['project_id']
+        boot = node_util.check_first_time_boot()
+        first_time = boot['first_time_boot']
     except:
         token = ""
         username=""
         project_id=""
         user_level="3"
+        first_time='FALSE'
 
 
 
-    return {'username': username, 
+    return {'username': username,
             'user_level': user_level,
             'project_id': project_id,
-            'token': token}
+            'token': token,
+            'first_time': first_time}
