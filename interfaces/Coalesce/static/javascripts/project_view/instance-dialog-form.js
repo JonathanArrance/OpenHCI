8a2032beabe4e7e3e6e6ec82b472cf53f330a914
Nick Brust <nick.b@transcirrus.com>
2014-05-21 18:52:46 -0400

    able to choose flavors

--- interfaces/Coalesce/static/javascripts/project_view/instance-dialog-form.js
+++ interfaces/Coalesce/static/javascripts/project_view/instance-dialog-form.js
@@ -40,6 +40,7 @@ $(function() {
 			image_name = $( "#image_name" ),
 			name= $( "#name" ),
 			network_name = $( "#network_name" ),
+                        flavor_name = $( "#flavor_name"),
 
 			allFields = $( [] ).add( sec_group_name ).add(sec_key_name).add(image_name).add(name).add(network_name),
 			tips = $( ".validateTips" );
@@ -80,7 +81,7 @@ $(function() {
 
 					if ( bValid ) {
 					  
-					   $.post('/create_image/' + name.val() + '/' + sec_group_name.val() + '/nova/m1.tiny/' + sec_key_name.val() + '/' + image_name.val() + '/' + network_name.val() + '/' + PROJECT_ID + '/');
+					   $.post('/create_image/' + name.val() + '/' + sec_group_name.val() + '/nova/' + flavor_name.val() + '/' + sec_key_name.val() + '/' + image_name.val() + '/' + network_name.val() + '/' + PROJECT_ID + '/');
 
 						$( this ).dialog( "close" );
 					}
