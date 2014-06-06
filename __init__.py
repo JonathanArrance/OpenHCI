e8adbc09f9088bcb6136dbde15f6160cc2f9cbc0
transuser <transuser@ciac-28490.(none)>
2014-06-06 11:56:19 -0400

    Removed the call to the logger listener to keep port conflict from happening

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
 import transcirrus.common.logger as logger
 
+# Call the function required to get everything setup for logging.
 logger.InitLogging()
-logger.StartConfigListener()
+
+# This function will setup the listener which listens for changes
+# to the logging config file and will reload the config when a 
+# change is made.
+# We aren't going to use this yet since we have some sort of
+# port conflict with something else. This needs to be fixed! 
+#logger.StartConfigListener()
