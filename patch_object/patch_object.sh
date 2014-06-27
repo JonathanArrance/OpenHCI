9cc9aba5bc49af174429dca43b308a55bb391648
Jonathan Arrance <jonathan@transcirrus.com>
2014-06-27 13:55:22 -0400

    added some things to the patch

--- patch_object/patch_object.sh
+++ patch_object/patch_object.sh
@@ -13,6 +13,9 @@ python2.7 /home/transuser/alpo_rhel/unittests/gluster_test.py
 #add the new valuse to the cinder table
 psql -U postgres -d transcirrus -c "UPDATE cinder_default SET param_value='AvailabilityZoneFilter,CapacityFilter,CapabilitiesFilter' WHERE parameter='scheduler_default_filters';"
 psql -U postgres -d transcirrus -c "INSERT INTO cinder_default VALUES ('storage_availability_zone', 'nova', 'cinder.conf');"
+psql -U postgres -d transcirrus -c "ALTER TABLE projects ALTER COLUMN def_network_id SET DEFAULT 0;"
+psql -U postgres -d transcirrus -c "ALTER TABLE trans_system_vols ADD COLUMN vol_type varchar(15)"
+psql -U postgres -d transcirrus -c "ALTER TABLE trans_system_vols ADD COLUMN vol_zone varchar"
 
 #update the cinder.conf file on the core node
 sed -i 's/scheduler_default_filters=AvailabilityZoneFilter/scheduler_default_filters=AvailabilityZoneFilter,CapacityFilter,CapabilitiesFilter/g' /etc/cinder/cinder.conf
@@ -20,3 +23,6 @@ echo "storage_availability_zone=nova" >> /etc/hosts
 
 #restart cinder
 python2.7 /home/transuser/alpo_rhel/unittests/service_test.py
+
+#add the volume types
+python2.7 /home/transuser/alpo_rhel/unittests/cinder_vol_test.py
