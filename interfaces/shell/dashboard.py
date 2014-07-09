5d24a1ef0619782d16c41c7755bdec78c1ae9624
Nick Brust <nick.b@transcirrus.com>
2014-07-09 16:15:37 -0400

    node work in shell

--- interfaces/shell/dashboard.py
+++ interfaces/shell/dashboard.py
@@ -134,28 +134,28 @@ def nodeInfo(d, node):
                     "Gluster Peer: " + node_info['node_gluster_peer'] + "\n" +
                     "Status: " + node_info['status']),
                     yes_label="Manage this Node",
-                    no_label="Return to Nodes", width=77, height=16)
+                    no_label="Return to Nodes", width=77, height=20)
 
 
 def nodeManage(d, node):
     node_info = node_op.get_node(node['node_id'])
     while True:
         elements = [
-            ("Name:", 1, 1, "", 1, 24, 16, 16, 0x2),
-            ("Id:", 2, 1, "", 2, 24, 16, 16, 0x2),
-            ("Type:", 3, 1, "", 3, 24, 16, 16, 0x2),
-            ("Data IP:", 4, 1, "", 4, 24, 16, 16, 0x2),
-            ("Management IP:", 5, 1, "", 5, 24, 16, 16, 0x0),
-            ("Controller:", 6, 1, "", 6, 24, 16, 16, 0x2),
-            ("Cloud Name:", 7, 1, "", 7, 24, 16, 16, 0x2),
-            ("Zone:", 8, 1, "", 8, 24, 16, 16, 0x2),
-            ("Fault:", 9, 1, "", 9, 24, 16, 16, 0x2),
-            ("Ready:", 10, 1, "", 10, 24, 16, 16, 0x2),
-            ("Gluster Peer:", 11, 1, "", 11, 24, 16, 16, 0x2),
-            ("Status:", 12, 1, "", 12, 24, 16, 16, 0x2)]
+            ("Name:", 1, 1, node_info['node_name'], 1, 24, 16, 16, 0x2),
+            ("Id:", 2, 1, node['node_id'], 2, 24, 16, 16, 0x2),
+            ("Type:", 3, 1, node_info['node_type'], 3, 24, 16, 16, 0x2),
+            ("Data IP:", 4, 1, node_info['node_data_ip'], 4, 24, 16, 16, 0x2),
+            ("Management IP:", 5, 1, node_info['node_mgmt_ip'], 5, 24, 16, 16, 0x0),
+            ("Controller:", 6, 1, node_info['node_controller'], 6, 24, 16, 16, 0x2),
+            ("Cloud Name:", 7, 1, node_info['node_cloud_name'], 7, 24, 16, 16, 0x2),
+            ("Zone:", 8, 1, node_info['availability_zone'], 8, 24, 16, 16, 0x2),
+            ("Fault:", 9, 1, node_info['node_fault_flag'], 9, 24, 16, 16, 0x2),
+            ("Ready:", 10, 1, node_info['node_ready_flag'], 10, 24, 16, 16, 0x2),
+            ("Gluster Peer:", 11, 1, node_info['node_gluster_peer'], 11, 24, 16, 16, 0x2),
+            ("Status:", 12, 1, node_info['status'], 12, 24, 16, 16, 0x2)]
 
         (code, fields) = d.mixedform(
-            "Update Node Info:", elements, width=77, height=16)
+            "Update Node Info:", elements, width=77, height=20)
 
         if handle_exit_code(d, code) == d.DIALOG_OK:
             break
