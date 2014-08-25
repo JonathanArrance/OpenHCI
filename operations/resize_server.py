9c3cefcb3d351ede8c61bb18cc5cc70ff1078227
Jonathan Arrance <jonathan@transcirrus.com>
2014-08-24 22:47:23 -0400

    added the resize server

---
+++ operations/resize_server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import sys
+import time
+
+import transcirrus.common.logger as logger
+import transcirrus.common.config as config
+import transcirrus.common.util as util
+
+import transcirrus.component.nova.server_action as sa
+import transcirrus.component.nova.server as server_ops
+
+def resize_and_confirm(auth_dict,server_dict):
+    """
+    DESC: The operations file definition will allow the resize and confirmation of the
+          resize, of the instance in the OpenStack cloud.
+    INPUT: auth_dict - authentication dictionary
+           server_dict - server_id
+                       - project_id
+                       - flavor_id
+    OUTPUT: OK - success
+    ACCESS: Only admins and power users can resize a server.
+    NOTE:None
+    """
+    action = sa(auth_dict)
+    server = server_ops(auth_dict)
+
+    #resize the server
+    resize = action.resize_server(server_dict)
+    if(resize == 'ERROR'):
+        logger.sys_error('The instance %s was not resized to flavor %s'%(server_dict['server_id'],server_dict[' flavor_id']))
+        raise Exception('The instance %s was not resized to flavor %s'%(server_dict['server_id'],server_dict[' flavor_id']))
+
+    #get the server status
+    while(status['server_status'] == 'RESIZE'):
+        logger.sys_info("Server is resizeing.")
+        status = server.get_server(server_dict)
+        logger.sys_info("Server status is %s."%(status['server_status']))
+
+    logge.sys_info('Sleeping for 60 seconds while instance %s stabilizes.'%(server_dict['server_id']))
+    time.sleep(60)
+
+    #we need to confirm the resize of the server
+    confirm = action.confirm_resize(server_dict)
+    if(confirm != 'OK'):
+        logger.sys_error('Could not confirm the instance resize.')
+        raise Exception('Could not confirm the instance resize.')
+
+    return 'OK'
