2cb43bf978d09448a652fd513562ef45584a4fe5
Nick Brust <nick.b@transcirrus.com>
2014-09-06 10:48:42 -0400

    removed single node checkbox from web interface

--- interfaces/Coalesce/coalesce/coal_beta/views.py
+++ interfaces/Coalesce/coalesce/coal_beta/views.py
@@ -1419,7 +1419,7 @@ def setup(request):
             mgmt_subnet            = form.cleaned_data['mgmt_subnet']
             mgmt_dns               = form.cleaned_data['mgmt_dns']
             #cloud_name             = form.cleaned_data['cloud_name']
-            single_node            = form.cleaned_data['single_node']
+            single_node            = False
             admin_password         = form.cleaned_data['admin_password']
             admin_password_confirm = form.cleaned_data['admin_password_confirm']
 
