f095cfd56591eef90a6fd7ee40a531bf7291c6ce
Jonathan Arrance <jonathan@transcirrus.com>
2014-09-06 19:33:29 -0400

    added a service control for nodes

--- common/service_control.py
+++ common/service_control.py
@@ -96,9 +96,7 @@ def cinder_sn(action):
     cinder_array1 = ['openstack-cinder-api', 'openstack-cinder-scheduler']
     out1 = _operator(cinder_array1,'stop')
     cinder_array = ['openstack-cinder-volume']
-    
     out = _operator(cinder_array,action)
-    
     return out
 
 def keystone(action):
@@ -294,6 +292,27 @@ def zero_connect_server(action):
     """
     zero_array = ['zero_connect_server']
     out = _operator(zero_array,action)
+    return out
+
+def zero_connect_node(action):
+    """
+    DESC: Control the pacemaker service
+    INPUT: start
+           restart
+           stop
+    OUTPUT: OK
+            ERROR
+            NA
+    ACCESS: Only an admin can control the pacemaker services.
+    NOTES:
+    """
+    out = None
+    if(config.NODE_TYPE == 'cn'):
+        zero_array = ['cn_zero_connect']
+        out = _operator(zero_array,action)
+    if(config.NODE_TYPE == 'sn'):
+        zero_array = ['sn_zero_connect']
+        out = _operator(zero_array,action)
     return out
 
 def dhcp_server(action):
