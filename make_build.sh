185cd22d1e008228a3f1f671c23110f86cb3bb4b
transuser <transuser@ciac-42649.(none)>
2014-11-06 16:15:42 -0500

    removed junk files

--- make_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-DATE=$(date +"%m-%d-%Y")
-git pull origin master
-tar -cvf alpo_rhat_${DATE}.tar ./common ./component ./core ./database ./ha ./__init__.py ./interfaces ./operations ./SQL_files
-#sudo mv ~/alpo_rhel/alpo_rhat_${DATE}.tar /var/www/builds/rhat/
-#sudo rm /var/www/builds/rhat/nightly
-#sudo ln -s /var/www/builds/rhat/alpo_rhat_${DATE}.tar /var/www/builds/rhat/nightly
