2462fa9a8517abdff632d4c28cb152d03eaa06fc
Jonathan Arrance <jonathan@transcirrus.com>
2015-01-29 17:18:45 -0500

    initial setup unit test script fixed

--- unittests/initial_setup_test.py
+++ unittests/initial_setup_test.py
@@ -11,20 +11,20 @@ auth = authorization("admin","password")
 b = auth.get_auth()
 
 new_system_variables = [
-    {"system_name":"ciac-27090","parameter":"api_ip","param_value":"192.168.10.52"},
-    {"system_name":"ciac-27090","parameter":"mgmt_ip","param_value":"192.168.168.52"},
-    {"system_name":"ciac-27090","parameter":"admin_api_ip","param_value":"192.168.10.52"},
-    {"system_name":"ciac-27090","parameter":"int_api_ip","param_value":"192.168.10.52"},
-    {"system_name":"ciac-27090","parameter":"uplink_ip","param_value":"192.168.10.52"},
-    {"system_name":"ciac-27090","parameter":"uplink_dns","param_value":"8.8.8.8"},
-    {"system_name":"ciac-27090","parameter":"uplink_gateway","param_value":"192.168.10.1"},
-    {"system_name":"ciac-27090","parameter":"uplink_domain_name","param_value":"rtp.transcirrus.com"},
-    {"system_name":"ciac-27090","parameter":"uplink_subnet","param_value":"255.255.255.0"},
-    {"system_name":"ciac-27090","parameter":"mgmt_domain_name","param_value":"int.transcirrus.com"},
-    {"system_name":"ciac-27090","parameter":"mgmt_subnet","param_value":"255.255.255.0"},
-    {"system_name":"ciac-27090","parameter":"mgmt_dns","param_value":"8.8.8.8"},
-    {"system_name":"ciac-27090","parameter":"vm_ip_min","param_value":"192.168.10.70"},
-    {"system_name":"ciac-27090","parameter":"vm_ip_max","param_value":"192.168.10.79"}
+    {"system_name":"ciac-38522","parameter":"api_ip","param_value":"192.168.10.52"},
+    {"system_name":"ciac-38522","parameter":"mgmt_ip","param_value":"192.168.168.52"},
+    {"system_name":"ciac-38522","parameter":"admin_api_ip","param_value":"192.168.10.52"},
+    {"system_name":"ciac-38522","parameter":"int_api_ip","param_value":"192.168.10.52"},
+    {"system_name":"ciac-38522","parameter":"uplink_ip","param_value":"192.168.10.52"},
+    {"system_name":"ciac-38522","parameter":"uplink_dns","param_value":"8.8.8.8"},
+    {"system_name":"ciac-38522","parameter":"uplink_gateway","param_value":"192.168.10.1"},
+    {"system_name":"ciac-38522","parameter":"uplink_domain_name","param_value":"rtp.transcirrus.com"},
+    {"system_name":"ciac-38522","parameter":"uplink_subnet","param_value":"255.255.255.0"},
+    {"system_name":"ciac-38522","parameter":"mgmt_domain_name","param_value":"int.transcirrus.com"},
+    {"system_name":"ciac-38522","parameter":"mgmt_subnet","param_value":"255.255.255.0"},
+    {"system_name":"ciac-38522","parameter":"mgmt_dns","param_value":"8.8.8.8"},
+    {"system_name":"ciac-38522","parameter":"vm_ip_min","param_value":"192.168.10.70"},
+    {"system_name":"ciac-38522","parameter":"vm_ip_max","param_value":"192.168.10.79"}
     ]
 
 yo = run_setup(new_system_variables,b)
