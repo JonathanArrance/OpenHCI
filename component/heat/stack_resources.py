bbb658eda3cad19142c45518f5f5a48e17178950
Jonathan Arrance <jonathan@transcirrus.com>
2015-02-13 21:02:38 -0500

    added the skeleton for heat code

---
+++ component/heat/stack_resources.py
@@ -0,0 +1,17 @@
+class stack_resources:
+    def find_stack_resources(self):
+        pass;
+    def list_resources(self):
+        pass;
+    def show_resource_data(self):
+        pass;
+    def show_resource_metadata(self):
+        pass;
+    def send_signal_to_resource(self):
+        pass;
+    def list_resource_types(self):
+        pass;
+    def show_resource_schema(self):
+        pass;
+    def show_resource_template(self):
+        pass;
