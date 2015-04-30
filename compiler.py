97e7abc51ee352c41e64f694988f965f0535a896
transuser <transuser@ciac-17312.(none)>
2015-04-30 16:01:41 -0400

    added full path in compiler

--- compiler.py
+++ compiler.py
@@ -1,15 +1,16 @@
 import compileall
 import os
-import glob
 
 
-folders = ['component','common','core','database','ha','interfaces','operations']
+folders = ['component','common','core','database','ha','operations']
 
 for folder in folders:
     print folder
-    compileall.compile_dir(folder,force=1)
+    compileall.compile_dir("/usr/local/lib/python2.7/transcirrus/" + folder,force=1)
 
-for root, dirs, files in os.walk("."):
+for root, dirs, files in os.walk("/usr/local/lib/python2.7/transcirrus"):
+    if(root == 'interfaces'):
+        continue
     for raw in files:
         f = raw.split('.')
         if(f[0] == 'config'):
@@ -17,3 +18,14 @@ for root, dirs, files in os.walk("."):
         else:
             os.system("sudo rm %s/%s.py"%(root,f[0]))
         #os.remove(glob.glob("%s/*.py"%(str(root))))
+
+
+compileall.compile_dir("/opt/Coalesce/" + folder,force=1)
+
+for root, dirs, files in os.walk("/opt/Coalesce"):
+    for raw in files:
+        f = raw.split('.')
+        if(f[0] == 'config'):
+            continue
+        else:
+            os.system("sudo rm %s/%s.py"%(root,f[0]))
