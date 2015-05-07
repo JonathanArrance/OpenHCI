f1788cbe44057de005542d7a84ace131478b60c7
rvalea <ricardo.valea@transcirrus.com>
2015-05-07 13:30:17 -0400

    wrong project ID for my Ceilometer test

--- unittests/ceilometer_meter_test.py
+++ unittests/ceilometer_meter_test.py
@@ -21,7 +21,7 @@ mo = meter_ops(b)
 print mo
 
 print "Listing meters"
-project_id = "6c1c12eb76b5407fa4d5bf9e150a5f0c"
+project_id = "9cf52d0ab22c4beab7baf3e84bb849dc"
 lm = mo.list_meters(project_id)
 print lm
 
