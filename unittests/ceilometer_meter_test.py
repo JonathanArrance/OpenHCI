60aa7362559c94f00034d352f62c330e71cbb072
rvalea <ricardo.valea@transcirrus.com>
2015-05-07 15:09:31 -0400

    testing out a bug

--- unittests/ceilometer_meter_test.py
+++ unittests/ceilometer_meter_test.py
@@ -27,28 +27,28 @@ print mo
 
 print "Show CPU percentage statistics for a given time frame"
 project_id = "10796d79f7124e0f8c9505b64bd8819d"
-start_time = "2015-05-07T13%3A00%3A00"
+start_time = "2015-05-05T13%3A00%3A00"
 end_time = "2015-05-07T14%3A00%3A00"
 cpustats = mo.show_cpu_percentage_statistics(project_id, start_time, end_time)
 print cpustats
 
 print "Show disk root usage statistics for a given time frame"
 project_id = "10796d79f7124e0f8c9505b64bd8819d"
-start_time = "2015-05-07T13%3A00%3A00"
+start_time = "2015-05-05T13%3A00%3A00"
 end_time = "2015-05-07T14%3A00%3A00"
 diskrootstats = mo.show_disk_root_usage_statistics(project_id, start_time, end_time)
 print diskrootstats
 
 print "Show memory usage statistics for a given time frame"
 project_id = "10796d79f7124e0f8c9505b64bd8819d"
-start_time = "2015-05-07T13%3A00%3A00"
+start_time = "2015-05-05T13%3A00%3A00"
 end_time = "2015-05-07T14%3A00%3A00"
 memoryusagestats = mo.show_memory_usage_statistics(project_id, start_time, end_time)
 print memoryusagestats
 
 print "Show VCPU statistics for a given time frame"
 project_id = "10796d79f7124e0f8c9505b64bd8819d"
-start_time = "2015-05-07T13%3A00%3A00"
+start_time = "2015-05-05T13%3A00%3A00"
 end_time = "2015-05-07T14%3A00%3A00"
 vcpustats = mo.show_vcpu_statistics(project_id, start_time, end_time)
 print vcpustats
