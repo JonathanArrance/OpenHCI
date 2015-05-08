183bd8ee3113898bf0e1f1117c1e469a7abbbe6c
Ricardo Valea <ricardo.valae@transcirrus.com>
2015-05-07 21:17:49 -0400

    testing a wild bug

--- component/ceilometer/ceilometer_meters.py
+++ component/ceilometer/ceilometer_meters.py
@@ -128,7 +128,7 @@ class meter_ops:
         else:
             util.http_codes(rest['response'],rest['reason'])
 
-    def show_statistics_for_meter_and_resource(self, project_id, start_time, end_time, meter_type, asdfdasfdsafadf):
+    def show_statistics_for_meter_and_resource(self, project_id, start_time, end_time, meter_type, resource_identifier):
         try:
             api_dict = {"username":self.username, "password":self.password, "project_id":project_id}
             if(project_id != self.project_id):
@@ -142,7 +142,8 @@ class meter_ops:
             body = ''
             header = {"X-Auth-Token":self.token, "Content-Type": "application/json", "Accept": "application/json", "User-Agent": "python-ceilometerclient"}
             function = 'GET'
-            api_path = '/v2/meters/' + meter_type + '/statistics?q.field=timestamp&q.field=timestamp&q.op=gt&q.op=le&q.type=&q.type=&q.value=' + start_time + '&q.value=' + end_time
+            # api_path = '/v2/meters/' + meter_type + '/statistics?q.field=timestamp&q.field=timestamp&q.op=gt&q.op=le&q.type=&q.type=&q.value=' + start_time + '&q.value=' + end_time
+            api_path = '/v2/meters/' + meter_type + '/statistics?q.field=resource&q.field=timestamp&q.field=timestamp&q.op=eq&q.op=gt&q.op=le&q.type=&q.type=&q.type=&q.value=' + resource_identifier + '&q.value=' + start_time + '&q.value=' + end_time
             token = self.token
             sec = 'FALSE'
             rest_dict = {"body": body, "header": header, "function": function, "api_path": api_path, "token": token, "sec": sec, "port": 8777}
