379ed72e777661a5e2348cf791a8353c2945357a
rvalea <ricardo.valea@transcirrus.com>
2015-05-12 11:06:40 -0400

    Clean up my code, resolved grammar errors, fixed indentation issues and provided better names for variables

--- component/ceilometer/ceilometer_meters.py
+++ component/ceilometer/ceilometer_meters.py
@@ -14,7 +14,7 @@ from transcirrus.database.postgres import pgsql
 
 class meter_ops:
 
-    def __init__(self,user_dict):
+    def __init__(self, user_dict):
         if(not user_dict):
             logger.sys_warning("No auth settings passed.")
             raise Exception("No auth settings passed")
@@ -36,14 +36,14 @@ class meter_ops:
             else:
                 self.sec = 'FALSE'
 
-            #used to overide the value in the DB, mostly used during setup or reinit
+            # used to overide the value in the DB, mostly used during setup or re init
             if('api_ip' in user_dict):
-                #NOTE may have to add an IP check
+                # NOTE may have to add an IP check
                 self.api_ip = user_dict['api_ip']
             else:
                 self.api_ip = config.API_IP
 
-            #get the default cloud controller info
+            # get the default cloud controller info
             self.controller = config.CLOUD_CONTROLLER
 
         if((self.username == "") or (self.password == "")):
@@ -58,15 +58,15 @@ class meter_ops:
             raise Exception("No tokens passed, or token was in error")
 
         if((self.status_level > 2) or (self.status_level < 0)):
-            logger.sys_error("Invalid status level passed for user: %s" %(self.username))
-            raise Exception("Invalid status level passed for user: %s" %(self.username))
+            logger.sys_error("Invalid status level passed for user: %s" % self.username)
+            raise Exception("Invalid status level passed for user: %s" % self.username)
 
     def list_meters(self, project_id):
         
         try:
-            api_dict = {"username":self.username, "password":self.password, "project_id":project_id}
+            api_dict = {"username": self.username, "password": self.password, "project_id": project_id}
             if(project_id != self.project_id):
-                self.token = get_token(self.username,self.password,self.project_id)
+                self.token = get_token(self.username, self.password, self.project_id)
             api = caller(api_dict)
         except:
             logger.sys_error("Could not connect to the Keystone API")
@@ -74,32 +74,38 @@ class meter_ops:
 
         try:
             body = ''
-            header = {"X-Auth-Token":self.token, "Content-Type": "application/json", "Accept": "application/json", "User-Agent": "python-ceilometerclient"}
+            header = {"X-Auth-Token": self.token, "Content-Type": "application/json", "Accept": "application/json", "User-Agent": "python-ceilometerclient"}
             function = 'GET'
             api_path = '/v2/meters'
             token = self.token
             sec = 'FALSE'
-            rest_dict = {"body": body, "header": header, "function": function, "api_path": api_path, "token": token, "sec": sec, "port": 8777}
-            if(self.api_ip):
+            rest_dict = {"body": body,
+                         "header": header,
+                         "function": function,
+                         "api_path": api_path,
+                         "token": token,
+                         "sec": sec,
+                         "port": 8777}
+            if self.api_ip:
                 rest_dict['api_ip'] = self.api_ip
             rest = api.call_rest(rest_dict)
         except:
             logger.sys_error("Could not list meters.")
             raise Exception("Could not list meters.")
 
-        if(rest['response'] == 200):
-            #read the json that is returned.
+        if rest['response'] == 200:
+            # read the json that is returned.
             logger.sys_info("Response %s with Reason %s" %(rest['response'],rest['reason']))
             load = json.loads(rest['data'])
             print load
         else:
-            util.http_codes(rest['response'],rest['reason'])
+            util.http_codes(rest['response'], rest['reason'])
 
     def show_statistics(self, project_id, start_time, end_time, meter_type, tenant_identifier=None, resource_identifier=None):
         try:
-            api_dict = {"username":self.username, "password":self.password, "project_id":project_id}
+            api_dict = {"username": self.username, "password": self.password, "project_id": project_id}
             if(project_id != self.project_id):
-                self.token = get_token(self.username,self.password,self.project_id)
+                self.token = get_token(self.username, self.password, self.project_id)
             api = caller(api_dict)
         except:
             logger.sys_error("Could not connect to the Keystone API")
@@ -119,8 +125,14 @@ class meter_ops:
                 api_path = '/v2/meters/' + meter_type + '/statistics?q.field=project&q.field=resource&q.field=timestamp&q.field=timestamp&q.op=eq&q.op=eq&q.op=gt&q.op=le&q.type=&q.type=&q.type=&q.type=&q.value=' + tenant_identifier + '&q.value=' + resource_identifier + '&q.value=' + start_time + '&q.value=' + end_time
             token = self.token
             sec = 'FALSE'
-            rest_dict = {"body": body, "header": header, "function": function, "api_path": api_path, "token": token, "sec": sec, "port": 8777}
-            if(self.api_ip):
+            rest_dict = {"body": body,
+                         "header": header,
+                         "function": function,
+                         "api_path": api_path,
+                         "token": token,
+                         "sec": sec,
+                         "port": 8777}
+            if self.api_ip:
                 rest_dict['api_ip'] = self.api_ip
             rest = api.call_rest(rest_dict)
 
@@ -128,10 +140,13 @@ class meter_ops:
             logger.sys_error("Could not list meters.")
             raise Exception("Could not list meters.")
 
-        if(rest['response'] == 200):
-            #read the json that is returned.
-            logger.sys_info("Response %s with Reason %s" %(rest['response'],rest['reason']))
+        if rest['response'] == 200:
+            # read the json that is returned.
+            logger.sys_info("Response %s with Reason %s" % (rest['response'], rest['reason']))
             load = json.loads(rest['data'])
-            print load
+
         else:
-            util.http_codes(rest['response'],rest['reason'])
+            logger.sys_error("Could not determine the meter type.")
+            raise Exception("Could not determine the meter type.")
+
+        return load
