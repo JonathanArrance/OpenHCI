ed70c1b4ff4f3ae6338c5032a1ee4d19e30dbd83
rvalea <ricardo.valea@transcirrus.com>
2015-06-05 11:24:30 -0400

    cleaned up the obtain memory operation and subsequent unit test

--- operations/obtain_memory_usage.py
+++ operations/obtain_memory_usage.py
@@ -1,6 +1,15 @@
 import os
+import time
 import transcirrus.common.logger as logger
 from transcirrus.component.nova.server import server_ops
+from transcirrus.component.ceilometer.ceilometer_mem_usage_patch import MemoryUtilization
+
+def daemonize(auth_dict, daemon_status, query_interval=None):
+    if query_interval is None:
+        query_interval = 10
+    while daemon_status:
+        time.sleep(query_interval)
+        get_mem_usage_for_instances(auth_dict)
 
 def get_mem_usage_for_instances(auth_dict):
     """
@@ -21,6 +30,12 @@ def get_mem_usage_for_instances(auth_dict):
             child_pid = os.fork()
             if child_pid == 0:
                 try:
-                    print entry['server_id']
+                    mu = MemoryUtilization(auth_dict)
+                    mu.manual_inspect_memory_usage(entry['os_ext_inst_name'], entry['project_id'], entry['server_id'])
+                    print entry['os_ext_inst_name'] + " || " + entry['project_id'] + " || " + entry['server_id']
+                except Exception as e:
+                    print ('Connection to mem patch failed: %s' % e)
+                    logger.sys_error('Connection to mem patch failed: %s' % e)
+                    raise Exception('Connection to mem patch failed: %s' % e)
                 finally:
-                    os.exit(0)
+                    os._exit(0)
--- unittests/ceilometer_operations_test.py
+++ unittests/ceilometer_operations_test.py
@@ -1,8 +1,13 @@
 from transcirrus.common.auth import authorization
+from transcirrus.common.auth import authorization
 import transcirrus.operations.obtain_memory_usage as memory_usage_ops
 
-print "Get mock instance list"
-instance_list = memory_usage_ops.mock_instance_list()
+print "Instantiating authorization object for an default admin"
+c = authorization("admin", "password")
+
+print "Get admin authorization dictionary"
+b = c.get_auth()
 
 print "Check elements of list"
-memory_usage_ops.get_mem_usage_for_instances(instance_list)
+memory_usage_ops.daemonize(b, True)
+
