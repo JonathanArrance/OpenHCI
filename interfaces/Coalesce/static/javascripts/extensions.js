b34058b040dd1ad5e7093bd0febddfa13d18fd3a
dcurrie <devin.currie@transcirrus.com>
2015-07-22 18:14:39 -0400

    added auto-fill to, removed old validation functions

--- interfaces/Coalesce/static/javascripts/extensions.js
+++ interfaces/Coalesce/static/javascripts/extensions.js
@@ -7,11 +7,11 @@ $.validator.setDefaults({
     errorElement: "span",
     errorClass: "help-block",
     highlight: function (element, errorClass, validClass) {
-        $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+        $(element).closest('.form-group, .form-group-sm, .form-group-lg').removeClass('has-success').addClass('has-error');
         $(element).attr('style', "border:rgb(217, 83, 79) 1px solid;");
     },
     unhighlight: function (element, errorClass, validClass) {
-        $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+        $(element).closest('.form-group, .form-group-sm, .form-group-lg').removeClass('has-error').addClass('has-success');
         $(element).attr('style', "border:rgb(92, 184, 92) 1px solid;");
     },
     errorPlacement: function (error, element) {
@@ -23,14 +23,21 @@ $.validator.setDefaults({
     }
 });
 
+// UI VALIDATION
 $(document).ready(function () {
     jQuery.validator.addMethod("password", function (value, element) {
         return this.optional(element) || value.length >= 8;
-    }, "Please use at least 8 characters");
+    }, "Please use at least 8 characters.");
+    jQuery.validator.addMethod("email", function (value, element) {
+        return this.optional(element) || /^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$/i.test(value);
+    }, "Please use a valid e-mail address.");
     jQuery.validator.addMethod("charField", function (value, element) {
         return this.optional(element) || /([0-9a-zA-Z_])+$/i.test(value);
-    }, "Please use letters, numbers and underscores(_)");
+    }, "Please use letters, numbers and underscores(_).");
     jQuery.validator.addMethod("ip", function(value, element) {
         return this.optional(element) || /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/i.test(value);
-    }, "Please enter valid IP address");
+    }, "Please enter valid IP address.");
+    jQuery.validator.addMethod("projectTag", function (value, element) {
+        return value != "";
+    }, "Tag cannot be blank to auto-fill form.");
 });
