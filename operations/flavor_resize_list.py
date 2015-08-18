3eb9a1e67831bfe498c87e7fe2d07c3ad01d81fe
rvalea <ricardo.valea@transcirrus.com>
2015-08-18 11:48:12 -0400

    created an operation that filters flavors for resizing based on current instance flavor size

---
+++ operations/flavor_resize_list.py
@@ -0,0 +1,24 @@
+from transcirrus.component.nova.server import server_ops
+from transcirrus.component.nova.flavor import flavor_ops
+
+def get_list_of_valid_resizeable_flavors(auth_dict, project_id, instance_id):
+
+    master_list = []
+    so = server_ops(auth_dict)
+    fo = flavor_ops(auth_dict)
+    instance_dictionary = {"server_id": instance_id, "project_id": project_id}
+    instance_info = so.get_server(instance_dictionary)
+    instance_flavor = instance_info["server_flavor"]
+    # flavor_id = instance_info["flavor_id"]
+    current_flavor_info = fo.get_flavor(instance_info["flavor_id"])
+    current_flavor_disk_size = current_flavor_info["disk_space(GB)"]
+
+    flavor_array = fo.list_flavors()
+    for flavor in flavor_array:
+        checking_flavor_disk = fo.get_flavor(flavor["id"])["disk_space(GB)"]
+        print checking_flavor_disk
+        if int(checking_flavor_disk) >= int(current_flavor_disk_size):
+            master_list.append(flavor)
+        else:
+            print "failed" + str(flavor)
+    return master_list
