ae3e3606450ff79c196ca975b216185d87d89c09
rvalea <ricardo.valea@transcirrus.com>
2015-08-18 14:56:13 -0400

    updated the resize instance feature to only show acceptable flavors to resize to.

--- unittests/flavor_resize_list_test.py
+++ unittests/flavor_resize_list_test.py
@@ -6,5 +6,5 @@ b = c.get_auth()
 
 instance_id = "b4892ad1-f8d0-48e0-a3f2-8ad6e182868e"
 project_id = "19d52f8bb4ea4647a6f394058ed7a984"
-result = flavor_resize_ops.get_list_of_valid_resizeable_flavors(b, project_id, instance_id)
+result = flavor_resize_ops.get_list_of_valid_resizeable_flavors(project_id, instance_id)
 print result
