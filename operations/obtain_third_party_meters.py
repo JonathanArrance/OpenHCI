847dde01b04d5b5baa9193ea99e45657c1d6f760
rvalea <ricardo.valea@transcirrus.com>
2015-08-25 17:17:52 -0400

    temp fix for collecting metrics

--- operations/obtain_third_party_meters.py
+++ operations/obtain_third_party_meters.py
@@ -10,14 +10,14 @@ def daemonize(daemon_status, query_interval=None):
     if query_interval is None:
         query_interval = 600
     while daemon_status:
-        # factory_creds = '/home/transuser/factory_creds'
-        # with open(factory_creds) as f:
-        #     content = [x.strip('export ').strip('\n') for x in f.readlines()]
-        #
-        # factory_creds_dict = dict(item.split("=") for item in content)
-        # c = authorization(factory_creds_dict['OS_USERNAME'], factory_creds_dict['OS_PASSWORD'])
-        # auth_dict = c.get_auth()
-        auth_dict = extras.shadow_auth()
+        factory_creds = '/home/transuser/factory_creds'
+        with open(factory_creds) as f:
+            content = [x.strip('export ').strip('\n') for x in f.readlines()]
+
+        factory_creds_dict = dict(item.split("=") for item in content)
+        c = authorization(factory_creds_dict['OS_USERNAME'], factory_creds_dict['OS_PASSWORD'])
+        auth_dict = c.get_auth()
+        # auth_dict = extras.shadow_auth()
         get_mem_usage_for_instances(auth_dict)
         get_disk_info_for_instances(auth_dict)
         get_memory_resident_for_instances(auth_dict)
@@ -37,7 +37,6 @@ def get_mem_usage_for_instances(auth_dict):
 
     so = server_ops(auth_dict)
     instance_list = so.list_all_servers()
-
     for i, entry in enumerate(instance_list):
         if entry['status'] == 'ACTIVE':
             child_pid = os.fork()
@@ -45,7 +44,6 @@ def get_mem_usage_for_instances(auth_dict):
                 try:
                     mu = ThirdPartyMeters(auth_dict)
                     mu.manual_inspect_memory_usage(entry['os_ext_inst_name'], entry['project_id'], entry['server_id'])
-                    # print entry['os_ext_inst_name'] + " || " + entry['project_id'] + " || " + entry['server_id']
                 except Exception as e:
                     print ('Connection to mem patch failed: %s' % e)
                     logger.sys_error('Connection to mem patch failed: %s' % e)
@@ -74,7 +72,6 @@ def get_disk_info_for_instances(auth_dict):
                 try:
                     mu = ThirdPartyMeters(auth_dict)
                     mu.manual_inspect_disk_info(entry['os_ext_inst_name'], entry['project_id'], entry['server_id'])
-                    # print entry['os_ext_inst_name'] + " || " + entry['project_id'] + " || " + entry['server_id']
                 except Exception as e:
                     print ('Connection to mem patch failed: %s' % e)
                     logger.sys_error('Connection to mem patch failed: %s' % e)
@@ -103,7 +100,6 @@ def get_memory_resident_for_instances(auth_dict):
                 try:
                     mu = ThirdPartyMeters(auth_dict)
                     mu.manual_inspect_memory_resident(entry['os_ext_inst_name'], entry['project_id'], entry['server_id'])
-                    # print entry['os_ext_inst_name'] + " || " + entry['project_id'] + " || " + entry['server_id']
                 except Exception as e:
                     print ('Connection to mem patch failed: %s' % e)
                     logger.sys_error('Connection to mem patch failed: %s' % e)
