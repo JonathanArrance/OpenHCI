830c2f0fcaea4a38888c9c6c4c400a8fa5296712
nbrust <nick.brust@transcirrus.com>
2015-11-02 12:56:44 -0500

    aPersona Updates
    updated properties file to work with local install (this info may need to be dynamically generated at setup time) and fixed an issue with aPersona scripts on the welcome page
    
    updated:
     * properties.py:
        * added rul and license keys that work with a local ASM install
     * welcome.html:
        * moved aPersona scripts into a block that will actually load them at runtime

--- interfaces/Coalesce/coalesce/coal_beta/properties.py
+++ interfaces/Coalesce/coalesce/coal_beta/properties.py
@@ -1,13 +1,13 @@
 """User specific properties to communicate with ASM."""
 
-ASM_URL = 'https://vault1.apersona.com:8443/apkv/{path}'
+ASM_URL = 'http://172.24.24.10:8080/apkv/{path}'
 
 COOKIE_NAME = 'trans-asm-py-d1a2n3c4e5%s%s'
 
-LICENSE_KEY_USER = 'aPAPI-4hfuru47fn347n387fn33'
+LICENSE_KEY_USER = 'tc-user-084rjt24p3ohtwqrpoif'
 
 #LICENSE_KEY_POWER_USER = 'aPAPI-TRANS-POWER-USER'
-LICENSE_KEY_POWER_USER = 'aPAPI-4hfuru47fn347n387fn33'
+LICENSE_KEY_POWER_USER = 'tc-admin-084rjt24p3ohtwqrpoif'
 
 
 class RequestAttributes(object):
