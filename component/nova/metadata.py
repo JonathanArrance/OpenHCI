54ee49045f2086189b465bd1ae28fa2940eb5c73
Brett Dolecheck <brett.dolecheck@transcirrus.com>
2016-01-13 13:18:57 -0500

    Added the ability to add exported shell variables to the user_data script that is uploaded to a newly created instance. The first exported variable (for now) is the user who created the instance which will be in the script variable called "USER".

--- component/nova/metadata.py
+++ component/nova/metadata.py
@@ -6,7 +6,7 @@ from transcirrus.component.glance.glance_ops_v2 import glance_ops
 # If the file xxx exists, then read it in and encode it to base64 and return the encoded string.
 # If the file does not exist then return and empty string "".
 # We use the metadata from the image to determine if we need to get a linux or windows version of the script.
-def get_user_data(image_id):
+def get_user_data(image_id, exports={}):
     auth = extras.shadow_auth()
     glance = glance_ops(auth)
     image_details = glance.get_image(image_id)
@@ -16,10 +16,26 @@ def get_user_data(image_id):
     if not os.path.exists(filename):
         return ("")
 
-    # Open and read in the entire file.
+    # Open and read in the entire file and replace the export section with any given exports.
     with open (filename, "r") as file:
-        data = file.read()
+        data=[]
+        for line in file:
+            if "<exports>" not in line:
+                data.append(line)
+            else:
+                if exports == {}:
+                    continue
+                if image_details['os_type'] == "linux":
+                    data.append("# Defined exports\n")
+                    for key, value in exports.iteritems():
+                        data.append('export %s="%s"\n' % (key,value))
 
-    b64_data = base64.b64encode(data)
+    buff = ''.join(data)
+    b64_buff = base64.b64encode(buff)
+    return (b64_buff)
 
-    return (b64_data)
+# Build a dictonary with the key/value pairs for any data we want to export to our user_data script.
+def build_export_data(username):
+    exports = {}
+    exports['USER'] = username
+    return (exports)
